c53ad9b8fa32ec610d843af0f1ddbdd6bf140f41
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-07-28 12:37:18 -0700

    lstm written

---
+++ src/Models/lstm.py
@@ -0,0 +1,33 @@
+# Standard libraries
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+# Importing the Keras libraries and packages
+from keras.model import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Get model
+def getModel(X_train):
+    model = Sequential()
+
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Droupout(0.2))
+
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(units = 50))
+    model.add(Dropout(0.2))
+
+    model.add(Dense(units = len(X_train[0])))
+
+    model.compile(optimizer = "adam", loss = 'mean_squared_error')
+
+    return model
+
