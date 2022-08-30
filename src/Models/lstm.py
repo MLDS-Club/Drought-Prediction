f3f6f196018f2cf3a20b3f8352df997e17724448
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-30 11:27:52 -0700

    updated

--- src/Models/lstm.py
+++ src/Models/lstm.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 # Importing the Keras libraries and packages
-from tensorflow.keras.model import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dropout
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 
 # Get model
 def getModel(X_train):
     model = Sequential()
-
-    model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+    
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 6)))
     model.add(Dropout(0.2))
 
     model.add(LSTM(units = 50, return_sequences = True))
-    model.add(Droupout(0.2))
+    model.add(Dropout(0.2))
 
     model.add(LSTM(units = 50, return_sequences = True))
     model.add(Dropout(0.2))
@@ -25,7 +25,7 @@ def getModel(X_train):
     model.add(LSTM(units = 50))
     model.add(Dropout(0.2))
 
-    model.add(Dense(units = len(X_train[0])))
+    model.add(Dense(units = 6))
 
     model.compile(optimizer = "adam", loss = 'mean_squared_error')
 
